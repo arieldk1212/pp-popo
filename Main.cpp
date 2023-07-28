01a55cf4bcf9f21159c95f7487ee8c22a2dc43d8
Ariel Kriheli <arieldk6@gmail.com>
2023-07-29 00:43:18 +0300

    added file creation functions

--- Main.cpp
+++ Main.cpp
@@ -1,31 +1,20 @@
-// main configuration for processes using pidstat | grep <program name>
-// output to txtfile containing the process info.
 #include <iostream>
-#include <fstream>
+#include "extras.h"
 
-typedef std::fstream FILE;
+using namespace EXR;
 
-void close_file(FILE* filename){
-  filename.close();
-}
-
-https://db.in.tum.de/teaching/ss20/c++praktikum/slides/lecture-12.pdf?lang=de
 int main() {
 
-  // std::system("pidstat | grep spotify >> ProcessInfo.txt");
+  std::cout << "Enter File Name";
 
-  FILE process_file;
+  string filename;
+  std::cin >> filename;
+  string* fptr = &filename;
 
-  process_file.open("processes.txt", std::ios::out);
+  File file;
+  file.filename = *fptr;
+  file.fCreation(*fptr);
 
-  if(!process_file){
-    std::cout << "Error in file creation.";
-  }else {
-    std::cout << "File Created";
-  }
-
-
-  close_file(&process_file);
 
   return 0;
 }
---
+++ extras.h
@@ -0,0 +1,72 @@
+#pragma once
+#include <string>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+
+
+namespace EXR {
+
+  typedef std::string string;
+
+  class File {
+  public:
+    string filename;
+
+    // this struct will contain data for the backend's DB desired Model.
+    struct Process {
+      int PID;
+      int UID;
+      double cpuPercent;
+      int CPU = 0;
+      string command;
+    };
+
+    void fCreation(string &filename) {
+      std::ofstream file(filename+".txt");
+      if(!file) {
+        std::cout << "Error While creating file - " << filename;
+      }else {
+        std::cout << "Created file - " << filename;
+      }
+      if(!file.is_open()) {
+        std::cout << "File Status Error";
+      }else {
+        file << std::system(("pidstat >> " + filename + ".txt").c_str());
+        file.close();
+      }
+    }
+  };
+
+
+  void getProcesses();
+
+
+  
+
+  void getProcesses();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
