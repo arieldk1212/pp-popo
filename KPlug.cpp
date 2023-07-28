e625760949ccaa6b8e1ba2ed5d951dc96a20a67d
Ariel Kriheli <arieldk6@gmail.com>
2023-07-28 16:29:06 +0300

    prepare proccesses cpp

---
+++ KPlug.cpp
@@ -0,0 +1,10 @@
+//File Made for kafak configuration and connection.
+#include <iostream>
+#include "librdkafka/rdkafkacpp.h"
+
+
+
+
+int main() {
+  return 0;
+}
---
+++ Main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+
+
+
+int main() {
+
+  std::cout << "Proccesses Viewer..";
+  
+  return 0;
+}
