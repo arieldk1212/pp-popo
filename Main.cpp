7193f6c2db7c25fcf3e4f13db8ba596d8c42b8f6
Ariel Kriheli <arieldk6@gmail.com>
2023-07-29 23:06:26 +0300

    added stuff

--- Main.cpp
+++ Main.cpp
@@ -2,6 +2,11 @@
 #include "afile.h"
 #include "tyepos.h"
 
+// https://linuxhint.com/check_memory_usage_process_linux/
+// https://www.programiz.com/dsa/binary-search-tree
+// https://www.programiz.com/dsa/hash-table
+// https://learn.microsoft.com/en-us/cpp/cpp/unions?view=msvc-170
+
 using namespace SYS;
 
 int main() {
